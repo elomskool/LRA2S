79966c484488f28a666d3669ae77db2d9d6d156a
elomskool <elomcarlo.toglo@galileivr.org>
2025-10-13 23:09:03 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -6,7 +6,7 @@ import os
 
 def main() -> None:
 
-    data = pd.read_csv('datasource/data.csv',  usecols = ['depth', 'distance'])
+    data = pd.read_csv('../datasource/data.csv',  usecols = ['depth', 'distance'])
 
     x = data['depth'].to_numpy()
     y = data['distance'].to_numpy()
@@ -73,4 +73,5 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
 
